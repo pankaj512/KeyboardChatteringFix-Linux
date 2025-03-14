d6f16b4e9af653113717abc565646ef5706e197a
Pankaj Kumar <pkmahan786@gmail.com>
2025-03-14 13:21:37 +0530

    add thresold for key a and key s

--- chattering_fix.sh
+++ chattering_fix.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 # Change the line below to the absolute path of the folder
-cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
+cd /home/pankaj/KeyboardChatteringFix-Linux && sudo /home/pankaj/.pyenv/bin/python3 -m src -k usb-SINO_WEALTH_Gaming_KB-event-kbd -t 100 --keys KEY_A,KEY_S
